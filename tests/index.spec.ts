712a8a4f7849d3225ebaa7b3cd5a8b944c33a028
taishi.inoue <ccpzjoh@gmail.com>
2024-04-11 13:50:20 +0900

    Add playwright from home

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -22,7 +22,61 @@ async function sendToSlack(username: string, text: string, channel: string) {
   }
 }
 
-test('get enyori and notify slack', async ({ page }) => {
+test('Get message from home and send slack', async ({ page }) => {
+  await page.goto(`${KIDS_VIEW_LOGIN_URL}`);
+
+  // login
+  await page.locator('#txtID').fill(`${KIDS_VIEW_ID}`);
+  await page.locator('#txtPASS').fill(`${KIDS_VIEW_PW}`);
+  await page.locator('#cmdLOGIN02').click();
+  const title = await page.locator('#lblEN_NAME');
+  await expect(title).toBeVisible();
+
+  // move to 家庭より
+  await page.locator('#grdMENU_ctl02_pnl_naka03').click();
+  const panel = await page.locator('#pnlKATEINOYOUSU');
+  await expect(panel).toBeVisible();
+
+  // 前日に移動
+  // await page.locator('#cmdBEFORE').click();
+  // const a = await page.locator('#pnlKATEINOYOUSU');
+  // await expect(a).toBeVisible();
+
+  const scraypeMessage = async () => {
+    const getSelectedValue = async (id: string) => {
+      return await page.$eval(id, v => v.options[v.options.selectedIndex].textContent)
+    }
+    const temp1 = await getSelectedValue('#ddlTEMPERATURE1')
+    const temp2 = await getSelectedValue('#ddlTEMPERATURE2')
+    const sleep11 = await getSelectedValue('#ddlSLEEP11')
+    const sleep12 = await getSelectedValue('#ddlSLEEP12')
+    const sleep21 = await getSelectedValue('#ddlSLEEP21')
+    const sleep22 = await getSelectedValue('#ddlSLEEP22')
+    const medicine = await getSelectedValue('#grdFREE_ctl02_ddlNAIYO')
+    const comment = await page.$eval('#txtCOMMENT', v => v.value);
+
+    if (!temp1) {
+      return ['今日の家庭からの入力がまだだよ']
+    } else {
+      return [
+        `体温: ${temp1}.${temp2}度`,
+        `睡眠: ${sleep11}:${sleep12} 〜 ${sleep21}:${sleep22}`,
+        `お薬: ${medicine}`,
+        `コメント: ${comment}`
+      ]
+    }
+  }
+
+  const messages = await scraypeMessage()
+  const date = await page.locator('#lblDATE').allInnerTexts();
+  await sendToSlack(
+    `${date} 家庭より`,
+    `\`\`\`${messages.join('\n')}\`\`\``,
+    '#保育園'
+  );
+})
+
+test('Get message from nursery and send slack', async ({ page }) => {
   await page.goto(`${KIDS_VIEW_LOGIN_URL}`);
 
   // login
@@ -34,10 +88,10 @@ test('get enyori and notify slack', async ({ page }) => {
 
   // move to 園より
   await page.locator('#grdMENU_ctl02_lnk02').click();
-  const title_en = await page.locator('#pnlENJINOYOUSU');
-  await expect(title_en).toBeVisible();
+  const panel = await page.locator('#pnlENJINOYOUSU');
+  await expect(panel).toBeVisible();
 
-  const scraypeMessageEnyori = async () => {
+  const scraypeMessage = async () => {
     const ELM_MAP = {
       '記入': '#lblSTAFF_NAME',
       '体温': '#lblTEMPERATURE1',
@@ -55,7 +109,7 @@ test('get enyori and notify slack', async ({ page }) => {
     return res
   }
 
-  const messages = await scraypeMessageEnyori()
+  const messages = await scraypeMessage()
   const date = await page.locator('#lblDATE').allInnerTexts();
   await sendToSlack(
     `${date} 保育園より`,
